ede8dcca329dae12599a7b9c80d6e51f2da17234
yangshaofeng <yangshaofeng@phoenixos.com>
2017-05-18 14:51:18 +0800

    aidl

---
+++ app/src/main/java/com/example/fewwind/chaozhuofirst/aidltest/ConfigMagagerImpl.java
@@ -0,0 +1,50 @@
+package com.example.fewwind.chaozhuofirst.aidltest;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+/**
+ * Created by fewwind on 17-5-18.
+ */
+
+public class ConfigMagagerImpl extends Binder implements IConfigManager{
+
+	private static final java.lang.String DESCRIPTPR = "configmanagerImp";
+
+
+	private String value;
+	private Context mContext;
+
+
+	public ConfigMagagerImpl(Context mContext) {
+		this.attachInterface(this,DESCRIPTPR);
+		this.mContext = mContext;
+	}
+
+
+	@Override public void setValue(String value) throws RemoteException {
+		this.value = value;
+	}
+
+
+	@Override public String getValue() throws RemoteException {
+		return value;
+	}
+	@Override public IBinder asBinder() {
+		return this;
+	}
+
+
+	@Override protected boolean onTransact(int code, Parcel data, Parcel reply, int flags)
+		throws RemoteException {
+
+		if (mContext.checkCallingOrSelfPermission("qw.qlm.ipctest.PERMISSION_CALL_REMOTE_SERVICE") == PackageManager.PERMISSION_DENIED){
+		}
+
+		return super.onTransact(code, data, reply, flags);
+	}
+}
---
+++ app/src/main/java/com/example/fewwind/chaozhuofirst/aidltest/IConfigManager.java
@@ -0,0 +1,13 @@
+package com.example.fewwind.chaozhuofirst.aidltest;
+
+import android.os.IInterface;
+import android.os.RemoteException;
+
+/**
+ * Created by fewwind on 17-5-18.
+ */
+
+public interface IConfigManager extends IInterface{
+	void setValue(String value) throws RemoteException;
+	String getValue() throws RemoteException;
+}
